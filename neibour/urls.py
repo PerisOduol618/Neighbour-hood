ded4fc056ece4618e7881dcb95a280a564be5c18
PerisOduol618 <oduolpepe618@gmail.com>
2020-11-02 21:44:31 +0300

    Add a url path fow the new neighbourhood

--- neibour/urls.py
+++ neibour/urls.py
@@ -7,4 +7,12 @@ from django.conf import settings
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('accounts/register/', views.register, name='register'),
+    path('profile/', views.profile,name = 'profile'),
+    path('update_profile/', user_views.update_profile,name = 'update_profile'),
+    path('new_hood/', views.new_hood, name='new_hood'),
+    path('hood/', views.neighbourhood, name = 'hood'),
+    path('edithood/', views.edit_hood, name= 'edithood')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
