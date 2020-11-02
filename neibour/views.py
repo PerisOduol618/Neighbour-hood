914461406196fd72a7cd884a33101fe55cf8b8bd
PerisOduol618 <oduolpepe618@gmail.com>
2020-11-02 21:40:59 +0300

    Add a new neighbourhood.html

--- neibour/views.py
+++ neibour/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
-from .forms import SignUpForm
+from .forms import SignUpForm, UserUpdateForm, ProfileUpdateForm,NewHoodForm,EditHoodForm
+from .models import Profile,Neighbourhood
 
 
 # Create your views here.
@@ -48,3 +50,77 @@ def profile(request):
         'profile_form': profile_form
     }
     return render(request, 'profile.html', context)
+
+
+
+
+
+@login_required(login_url='/accounts/login/')
+def update_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user)
+
+        if user_form.is_valid() and profile_form.is_valid() :
+            user_form.save()
+            profile_form.save()
+
+            return redirect('profile')
+
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user)
+
+        context = {
+            'user_form': user_form,
+            'profile_form': profile_form
+
+        }
+
+    return render(request, 'update_profile.html', context)
+
+
+
+
+@login_required(login_url='/accounts/login/')
+def neighbourhood(request):
+    hoods = Neighbourhood.objects.all()
+    return render(request,'neighbourhoods.html',{"hoods":hoods})
+
+
+
+
+@login_required(login_url='/accounts/login/')
+def new_hood(request):   
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewHoodForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.admin = current_user.profile 
+           
+            image.save()
+            
+        return redirect('hood')
+
+    else:
+        form = NewHoodForm()
+    return render(request, 'new_hood.html', {"form": form})
+
+
+def edit_hood(request):
+    current_user = request.user
+    if request.method =='POST':
+        form = EditHoodForm(request.POST, request.FILES, instance = request.user)
+        if form.is_valid():
+            image = form.save(commit = False)
+            image.admin = current_user.profile
+
+            image.save()
+        return redirect('hood')
+    
+    else:
+        form = EditHoodForm()
+    return render(request,'edit_hood.html',{'form':form})
