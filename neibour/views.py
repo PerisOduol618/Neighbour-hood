c5be6d819d8eab0d30dd8234c0897f4aea597bf6
PerisOduol618 <oduolpepe618@gmail.com>
2020-11-03 20:37:50 +0300

    Add a join and leave function

--- neibour/views.py
+++ neibour/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render,redirect
-from django.http  import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
-from .forms import SignUpForm, UserUpdateForm,ProfileUpdateForm,NewHoodForm,EditHoodForm, NewBizForm,NewPostForm
-from .models import Profile,Neighbourhood,Business,Post
+from .forms import SignUpForm, UserUpdateForm, ProfileUpdateForm, NewHoodForm, EditHoodForm, NewBizForm, NewPostForm
+from .models import Profile, Neighbourhood, Business, Post
 
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
 
 
 def register(request):
@@ -27,19 +27,18 @@ def register(request):
     return render(request, 'registration/registration_form.html', {'form': form})
 
 
-
-
-
 @login_required(login_url='/accounts/login/')
 def profile(request):
     if request.method == 'POST':
         user_form = UserUpdateForm(request.POST, instance=request.user)
-        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        profile_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
 
         if user_form.is_valid and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            messages.success(request,f'Your account has been updated successfully!')
+            messages.success(
+                request, f'Your account has been updated successfully!')
             return redirect('profile')
     else:
         user_form = UserUpdateForm(instance=request.user)
@@ -51,18 +50,16 @@ def profile(request):
     return render(request, 'profile.html', context)
 
 
-
-
-
 @login_required(login_url='/accounts/login/')
 def update_profile(request):
     current_user = request.user
     if request.method == 'POST':
 
         user_form = UserUpdateForm(request.POST, instance=request.user)
-        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user)
+        profile_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user)
 
-        if user_form.is_valid() and profile_form.is_valid() :
+        if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
 
@@ -80,24 +77,24 @@ def update_profile(request):
 
     return render(request, 'update_profile.html', context)
 
+
 @login_required(login_url='/accounts/login/')
 def hood(request):
     hoods = Neighbourhood.objects.all()
-    return render(request,'neighbourhoods.html',{"hoods":hoods})
-
+    return render(request, 'neighbourhoods.html', {"hoods": hoods})
 
 
 @login_required(login_url='/accounts/login/')
-def new_hood(request):   
+def new_hood(request):
     current_user = request.user
     if request.method == 'POST':
         form = NewHoodForm(request.POST, request.FILES)
         if form.is_valid():
             image = form.save(commit=False)
             image.admin = current_user.profile
-           
+
             image.save()
-            
+
         return redirect('hood')
 
     else:
@@ -107,76 +104,83 @@ def new_hood(request):
 
 def edit_hood(request):
     current_user = request.user
-    if request.method =='POST':
-        form = EditHoodForm(request.POST, request.FILES, instance = request.user)
+    if request.method == 'POST':
+        form = EditHoodForm(request.POST, request.FILES, instance=request.user)
         if form.is_valid():
-            image = form.save(commit = False)
+            image = form.save(commit=False)
             image.admin = current_user.profile
 
             image.save()
         return redirect('hood')
-    
+
     else:
         form = EditHoodForm()
-    return render(request,'edit_hood.html',{'form':form})
+    return render(request, 'edit_hood.html', {'form': form})
 
 
+def joinhood(request, id):
+    hood = get_object_or_404(Neighbourhood, id=id)
+    request.user.profile.neighbourhood = hood
+    request.user.profile.save()
+    return redirect('hood')
 
 
-def joinhood(request,id):
-    hood = get_object_or_404(Neighbourhood,id = id)
-    request.user.profile.hood =hood
+def leavehood(request, id):
+    hood = get_object_or_404(Neighbourhood, id=id)
+    request.user.profile.neighbourhood = None
     request.user.profile.save()
     return redirect('hood')
 
 
 @login_required(login_url='/accounts/login/')
 def singlehood(request, id):
-    businesses = Business.hood_biz(id = id)
-    post = Post.hood_post(id = id)
-    return render(request, 'singlehood.html', {'businesses':businesses, 'post':post})
-    
+    hood = Neighbourhood.objects.get(id=id)
+    return render(request, 'singlehood.html', {'hood':hood})
+
+
+@login_required(login_url='/accounts/login/')
+def businesses(request, id):
+    business = Business. hood_biz(id=id)
+    return render(request, 'business.html', {'business': business})
+
 
 @login_required(login_url='/accounts/login/')
 def newbiz(request):
     current_user = request.user
     if request.method == 'POST':
-        form =  NewBizForm(request.POST, request.FILES)
+        form = NewBizForm(request.POST, request.FILES)
         if form.is_valid():
             business = form.save(commit=False)
             business.user = current_user
-           
+
             business.save()
-            
+
         return redirect('hood')
 
     else:
-        form =  NewBizForm()
+        form = NewBizForm()
     return render(request, 'newbiz.html', {"form": form})
 
 
 @login_required(login_url='/accounts/login/')
 def posthood(request, id):
-    post = Post.hood_post(id = id)
-    return render(request, 'hoodpost.html', {'post':post})
-
-
-
+    post = Post.hood_post(id=id)
+    return render(request, 'hoodpost.html', {'post': post})
 
 
 @login_required(login_url='/accounts/login/')
 def post(request):
     current_user = request.user
     if request.method == 'POST':
-        form =  NewPostForm(request.POST, request.FILES)
+        form = NewPostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.user = current_user
-           
+
             post.save()
-            
+
         return redirect('hood')
 
     else:
-        form =  NewPostForm()
+        form = NewPostForm()
     return render(request, 'post.html', {"form": form})
